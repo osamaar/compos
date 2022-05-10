74c55c7c8f42b84164458798c8b7e7c21560826b
osamagdi <osama.arafa@outlook.com>
2022-05-10 19:46:23 -0400

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,58 +4,6 @@
 #include <typeindex>
 
 #define UNUSED(expr) do { (void)(expr); } while (0)
-
-int I = 0;
-
-int fn() {
-    static int i = I++;
-    return i;
-}
-
-// int test() {
-//     using namespace ecs;
-
-//     ComponentProvider provider;
-//     Archetype arche {provider};
-//     Archetype arche1 {provider};
-
-//     arche.add_component<CompGeometry>();
-//     arche.add_component<CompGeometry>();
-//     arche.add_component<CompVisual>();
-
-//     arche1.add_component<CompGeometry>();
-//     arche1.add_component<CompVisual>();
-//     arche1.add_component<CompUserController>();
-
-//     typeid_t c0 = CompGeometry::uuid();
-//     typeid_t c1 = CompVisual::uuid();
-//     typeid_t c2 = CompUserController::uuid();
-
-//     typeid_t compcount = UUID<Component>::id_counter;
-
-//     typeid_t a0 = arche.uuid();
-//     typeid_t a1 = arche1.uuid();
-
-//     typeid_t archecount = UUID<Archetype>::id_counter;
-    
-//     return 0;
-// }
-
-// struct Component {
-// };
-// template <typename T>
-// struct ComponentBase: public Component {
-//     static const typeid_t uuid() {
-//         return UUID<Component>::get<T>();
-//     }
-
-//     // template <typename... Args>
-//     // static void construct(void *memory, Args... args) {
-//     //     ::new (memory) T{std::forward(args)...};
-//     // }
-// };
-
-// struct CompGeometry : public ComponentBase<CompGeometry> {
 struct CompGeometry {
     int x; int y;
 };
